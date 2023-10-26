99aea381a0d8b48f28c9c35d5647da536799c2ad
bcaro57 <bencaro@udel.edu>
2023-10-26 19:28:59 -0400

    starting to make the robot reciever code

--- RobotReciever/src/main.cpp
+++ RobotReciever/src/main.cpp
@@ -1,18 +1,72 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <RH_RF95.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Feather 32u4:
+#define RFM95_CS   8
+#define RFM95_RST  4
+#define RFM95_INT  7
+
+// Operating frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  Serial.begin(115200);
+  // while (!Serial) delay(1);
+  // delay(100);
+
+  Serial.println("Feather LoRa RX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  rf95.setTxPower(23, false);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  if (rf95.available()) {
+    // Should be a message for us now
+    uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+    uint8_t len = sizeof(buf);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (rf95.recv(buf, &len)) {
+      digitalWrite(LED_BUILTIN, HIGH);
+      RH_RF95::printBuffer("Received: ", buf, len);
+      Serial.print("Got: ");
+      Serial.println((char*)buf);
+       Serial.print("RSSI: ");
+      Serial.println(rf95.lastRssi(), DEC);
+
+      // Send a reply
+      uint8_t data[] = "And hello back to you";
+      rf95.send(data, sizeof(data));
+      rf95.waitPacketSent();
+      Serial.println("Sent a reply");
+      digitalWrite(LED_BUILTIN, LOW);
+    } else {
+      Serial.println("Receive failed");
+    }
+  }
 }
