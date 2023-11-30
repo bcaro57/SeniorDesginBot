b972cbd046454a7ef2d9480a988c5e4d3ee060d4
bcaro57 <bencaro@udel.edu>
2023-11-30 16:45:00 -0500

    synced test code with this code

--- HomeBase/src/main.cpp
+++ HomeBase/src/main.cpp
@@ -2,114 +2,98 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
-#define RFM95_CS    8
-#define RFM95_INT   7
-#define RFM95_RST   4
+// Feather 32u4:
+#define RFM95_CS   8
+#define RFM95_RST  4
+#define RFM95_INT  7
 
-#define SENSOR_PIN 0
-
-// operating frequency, must match RX's freq!
+// Operating frequency, must match RX's freq!
 #define RF95_FREQ 915.0
 
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
+// Defining the pins being used
+#define ALL_BUTTON 9
+#define BUTTON_PIN_RIGHT 10
+#define BUTTON_PIN_MIDDLE 11
+#define BUTTON_PIN_LEFT 12
+
+
 void setup() {
+  // for the button
+  pinMode(ALL_BUTTON, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_RIGHT, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_MIDDLE, INPUT_PULLUP);
+  pinMode(BUTTON_PIN_LEFT, INPUT_PULLUP);
+
+  // for the radio stuff
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
-
   Serial.begin(115200);
-  // while (!Serial) delay(1);
-  // delay(100);
-
+  delay(100);
   Serial.println("Feather LoRa TX Test!");
-
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
   digitalWrite(RFM95_RST, HIGH);
   delay(10);
-
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
     Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
   Serial.println("LoRa radio init OK!");
-
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   if (!rf95.setFrequency(RF95_FREQ)) {
     Serial.println("setFrequency failed");
     while (1);
   }
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+  // you can set transmitter powers from 5 to 23 dBm:
   rf95.setTxPower(23, false);
 }
 
-int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void loop() {
-  int sensorValue = analogRead(SENSOR_PIN);
-
-  if (sensorValue < 512){
-    int reversePWM = -(sensorValue - 511) / 2; 
-    int Left_Output = 0;
-    int Right_Output = reversePWM;
-
-    rf95.send((uint8_t *)Left_Output, 20);
-    rf95.send((uint8_t *)Right_Output, 20);
+  int delayTime = 10;
+  
+  uint8_t buttonValue[1];
+  if (digitalRead(BUTTON_PIN_LEFT) == LOW ) {
+    Serial.print("The left button is being pressed and the value is ");
+    buttonValue[0] = 0x01;
+    Serial.println(buttonValue[0]);
+    delay(delayTime);
   }
-
+  else if (digitalRead(BUTTON_PIN_MIDDLE) == LOW ) {
+    Serial.print("The middle button is being pressed and the value is ");
+    buttonValue[0] = 0x02;
+    Serial.println(buttonValue[0]);
+    delay(delayTime);
+  }
+  else if (digitalRead(BUTTON_PIN_RIGHT) == LOW ) {
+    Serial.print("The right button is being pressed and the value is ");
+    buttonValue[0] = 0x03;
+    Serial.println(buttonValue[0]);
+    delay(delayTime);
+  }
+  else if (digitalRead(ALL_BUTTON) == LOW ) {
+    Serial.print("The 'all' button is being pressed and the value is ");
+    buttonValue[0] = 0x04;
+    Serial.println(buttonValue[0]);
+    delay(delayTime);
+  }
   else {
-    int forwardPWM = (sensorValue - 512) / 2; 
-    int Left_Output = forwardPWM;
-    int Right_Output = 0;
-
-    rf95.send((uint8_t *)Left_Output, 20);
-    rf95.send((uint8_t *)Right_Output, 20);
-  }
-
-  delay(10);
-
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);  
-
-  if (rf95.recv(buf, &len)) {
-
-  }
-
-
-  delay(1000); // Wait 1 second between transmits, could also 'sleep' here!
-  Serial.println("Transmitting..."); // Send a message to rf95_server
-
-  char radiopacket[20] = "Hello World #      ";
-  itoa(packetnum++, radiopacket+13, 10);
-  Serial.print("Sending "); Serial.println(radiopacket);
-  radiopacket[19] = 0;
-
-  Serial.println("Sending...");
-  delay(10);
-  rf95.send((uint8_t *)radiopacket, 20);
-
-  Serial.println("Waiting for packet to complete...");
-  delay(10);
-  rf95.waitPacketSent();
-  // Now wait for a reply
-  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-  Serial.println("Waiting for reply...");
-  if (rf95.waitAvailableTimeout(1000)) {
-    // Should be a reply message for us now
-    if (rf95.recv(buf, &len)) {
-      Serial.print("Got reply: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf95.lastRssi(), DEC);
-    } else {
-      Serial.println("Receive failed");
+    Serial.print("The buttons are not being pressed and the value is ");
+    buttonValue[0] = 0x00;
+    Serial.println(buttonValue[0]);
+    delay(delayTime);
     }
-  } else {
-    Serial.println("No reply, is there a listener around?");
-  }
-
+  rf95.send((uint8_t *)buttonValue, 1);
+  rf95.waitPacketSent();
 }
